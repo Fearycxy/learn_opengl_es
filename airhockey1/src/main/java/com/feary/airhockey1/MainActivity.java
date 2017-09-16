e11010e3a2521405ee3a8dbab0bc9743808d06d1
feary <fearycxy@gmail.com>
2017-09-17 00:56:48 +0800

    module change

---
+++ airhockey1/src/main/java/com/feary/airhockey1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.feary.airhockey1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feary.airhockey2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
