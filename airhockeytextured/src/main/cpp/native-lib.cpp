abc5630a4000ab4ff5f87141a4f562608486d59b
caoxiangyu <caoxiangyu@xiaomi.com>
2017-09-26 13:24:00 +0800

    test

--- airhockeytextured/src/main/cpp/native-lib.cpp
+++ airhockeytextured/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_com_feary_airhockey_AirHockeyActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Java_com_feary_airhockey_AirHockeyActivity_stringFromJNI++";
     return env->NewStringUTF(hello.c_str());
 }
